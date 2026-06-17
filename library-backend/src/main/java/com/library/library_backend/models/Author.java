7d62b7b29c9a3ade1418c3fdbbf4263ef49141c6
Frasato <gabriel.frasato@hotmail.com>
2026-06-17 17:02:15 -0300

    feat: create author model

---
+++ library-backend/src/main/java/com/library/library_backend/models/Author.java
@@ -0,0 +1,20 @@
+package com.library.library_backend.models;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "autores")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String nome;
+    private String dataNasc;
+    private String bio;
+    @OneToMany(mappedBy = "author")
+    private List<Book> livro;
+
+}
