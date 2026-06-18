a223dca999262ee6227d31dd9ddc8aa6198171da
Frasato <gabriel.frasato@hotmail.com>
2026-06-17 21:52:06 -0300

    feat: add getters and setters

--- library-backend/src/main/java/com/library/library_backend/models/Book.java
+++ library-backend/src/main/java/com/library/library_backend/models/Book.java
@@ -7,17 +7,16 @@ import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table(name = "livros")
+@Table(name = "livro")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String titulo;
     private Date dataPublicacao;
-    private String isbn;
-    private String editora;
-    private Timestamp createdAt;
-    private Timestamp updatedAt;
+    private List<String> isbn;
+    private List<String> editora;
+    private String createdAt;
 
     @ManyToMany
     @JoinTable(
@@ -27,8 +26,80 @@ public class Book {
     )
     private List<Book> livrosSemelhantes;
 
-    @ManyToOne
+    @ManyToMany
     @JoinColumn(name = "author_id")
-    private Author author;
+    private List<Author> author;
 
+    public Book() {}
+
+    public Book(UUID id, String titulo, Date dataPublicacao, List<String> isbn, List<String> editora, String createdAt, List<Book> livrosSemelhantes, List<Author> author) {
+        this.id = id;
+        this.titulo = titulo;
+        this.dataPublicacao = dataPublicacao;
+        this.isbn = isbn;
+        this.editora = editora;
+        this.createdAt = createdAt;
+        this.livrosSemelhantes = livrosSemelhantes;
+        this.author = author;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Date getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    public void setDataPublicacao(Date dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    public List<String> getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(List<String> isbn) {
+        this.isbn = isbn;
+    }
+
+    public List<String> getEditora() {
+        return editora;
+    }
+
+    public void setEditora(List<String> editora) {
+        this.editora = editora;
+    }
+
+    public String getCreatedAt() {return createdAt;}
+
+    public void setCreatedAt(String createdAt) {this.createdAt = createdAt;}
+
+    public List<Book> getLivrosSemelhantes() {
+        return livrosSemelhantes;
+    }
+
+    public void setLivrosSemelhantes(List<Book> livrosSemelhantes) {
+        this.livrosSemelhantes = livrosSemelhantes;
+    }
+
+    public List<Author> getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(List<Author> author) {
+        this.author = author;
+    }
 }
