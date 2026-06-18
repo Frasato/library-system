1bcc9972f403031609078740150522e5171aade5
Frasato <gabriel.frasato@hotmail.com>
2026-06-18 15:33:43 -0300

    refactor: change service name

--- library-backend/src/main/java/com/library/library_backend/services/BookByIsbnService.java
+++ library-backend/src/main/java/com/library/library_backend/services/BookByIsbnService.java
@@ -1,40 +1,33 @@
 package com.library.library_backend.services;
 
 import com.library.library_backend.dto.BookResponseOpenLibraryDto;
-import com.library.library_backend.models.Book;
-import com.library.library_backend.repositories.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public class BookService {
-    @Autowired
-    private BookRepository bookRepository;
-
+public class BookByIsbnService {
     private final RestTemplate restTemplate;
 
-    public BookService(RestTemplate restTemplate){
+    public BookByIsbnService(RestTemplate restTemplate){
         this.restTemplate = restTemplate;
     }
 
-    public Book getBookByIsbn(String isbn){
+    public BookResponseOpenLibraryDto fetch(String isbn){
         if(isbn.isEmpty()) throw new RuntimeException("ISBN can't be empty!");
 
         try{
             String uri = "https://openlibrary.org/isbn/"+ isbn +".json";
-            BookResponseOpenLibraryDto responseOpenLibraryDto = restTemplate.getForObject(uri, BookResponseOpenLibraryDto.class);
+            BookResponseOpenLibraryDto response = restTemplate.getForObject(uri, BookResponseOpenLibraryDto.class);
 
-            if(responseOpenLibraryDto != null){
-                return responseOpenLibraryDto.toEntity();
+            if(response != null){
+                return response;
             }else{
                 throw new RuntimeException("No body founded");
             }
 
         }catch (RestClientException e){
-            throw new RuntimeException(e.getMessage());
+            throw new RuntimeException(e.getMessage() + " | " + e.getCause());
         }
     }
-
 }
