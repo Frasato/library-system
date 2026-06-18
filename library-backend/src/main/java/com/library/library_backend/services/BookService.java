f149c07314bd46bfa7081d6737f27c3f9ae0485e
Frasato <gabriel.frasato@hotmail.com>
2026-06-17 21:52:47 -0300

    feat: add function to fetch book from isbn code

--- library-backend/src/main/java/com/library/library_backend/services/BookService.java
+++ library-backend/src/main/java/com/library/library_backend/services/BookService.java
@@ -1,9 +1,11 @@
 package com.library.library_backend.services;
 
+import com.library.library_backend.dto.BookResponseOpenLibraryDto;
 import com.library.library_backend.models.Book;
 import com.library.library_backend.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -18,10 +20,21 @@ public class BookService {
     }
 
     public Book getBookByIsbn(String isbn){
-        if(isbn.isEmpty()) throw new RuntimeException("ISBN não pode ser vazio!");
+        if(isbn.isEmpty()) throw new RuntimeException("ISBN can't be empty!");
 
-        String uri = "https://openlibrary.org/isbn/"+ isbn +".json";
+        try{
+            String uri = "https://openlibrary.org/isbn/"+ isbn +".json";
+            BookResponseOpenLibraryDto responseOpenLibraryDto = restTemplate.getForObject(uri, BookResponseOpenLibraryDto.class);
 
+            if(responseOpenLibraryDto != null){
+                return responseOpenLibraryDto.toEntity();
+            }else{
+                throw new RuntimeException("No body founded");
+            }
+
+        }catch (RestClientException e){
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
 }
