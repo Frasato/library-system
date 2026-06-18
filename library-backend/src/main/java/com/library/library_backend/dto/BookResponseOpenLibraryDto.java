65f90c6262f4294ee766286bc02964791d1a8e02
Frasato <gabriel.frasato@hotmail.com>
2026-06-18 15:42:35 -0300

    cleanup: remove unnecessary codes

--- library-backend/src/main/java/com/library/library_backend/dto/BookResponseOpenLibraryDto.java
+++ library-backend/src/main/java/com/library/library_backend/dto/BookResponseOpenLibraryDto.java
@@ -1,30 +1,12 @@
 package com.library.library_backend.dto;
 
-import com.library.library_backend.models.Book;
-
 import java.util.List;
 
 public record BookResponseOpenLibraryDto(
         String title,
-        List<AuthorKey> authors,
+        List<AuthorKeyDto> authors,
+        List<WorksDto> works,
         List<String> publishers,
         List<String> isbn_13,
-        Created created
-){
-    public Book toEntity(){
-        Book book = new Book();
-        book.setTitulo(this.title);
-        book.setEditora(this.publishers);
-        book.setIsbn(this.isbn_13);
-        book.setCreatedAt(this.created.value());
-        return book;
-    }
-}
-
-record AuthorKey(
-        String key
-){}
-
-record Created(
-        String value
+        String publish_date
 ){}
