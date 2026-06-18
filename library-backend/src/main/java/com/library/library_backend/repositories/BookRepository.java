abf5d475097ac7403448a60801c1d09714b17223
Frasato <gabriel.frasato@hotmail.com>
2026-06-18 15:37:10 -0300

    feat: add book repository

--- library-backend/src/main/java/com/library/library_backend/repositories/BookRepository.java
+++ library-backend/src/main/java/com/library/library_backend/repositories/BookRepository.java
@@ -4,7 +4,10 @@ import com.library.library_backend.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, UUID> {}
+public interface BookRepository extends JpaRepository<Book, UUID> {
+    Optional<Book> findByIsbn(String isbn);
+}
