11b816d2e6f3e5e9491dedef9aaa9e2b351d939d
Frasato <gabriel.frasato@hotmail.com>
2026-06-19 14:16:03 -0300

    feat: add endpoint to delete a book

--- library-backend/src/main/java/com/library/library_backend/controllers/BookController.java
+++ library-backend/src/main/java/com/library/library_backend/controllers/BookController.java
@@ -1,14 +1,15 @@
 package com.library.library_backend.controllers;
 
+import com.library.library_backend.dto.RequestDeleteBookDto;
 import com.library.library_backend.dto.RequestIsbnDto;
 import com.library.library_backend.dto.ResponseBookDto;
+import com.library.library_backend.dto.ResponseDeleteBookDto;
 import com.library.library_backend.models.Book;
 import com.library.library_backend.services.BookFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -34,5 +35,11 @@ public class BookController {
     public ResponseEntity<ResponseBookDto> createBook(@RequestBody RequestIsbnDto requestIsbnDto){
         ResponseBookDto response = bookFacade.createNewBook(requestIsbnDto.isbn());
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ResponseDeleteBookDto> deleteBook(@PathVariable("id") RequestDeleteBookDto requestDeleteBookDto){
+        ResponseDeleteBookDto response = bookFacade.deleteBook(requestDeleteBookDto.id());
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
