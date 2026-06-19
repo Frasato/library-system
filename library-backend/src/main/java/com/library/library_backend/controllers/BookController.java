8b4e2b00191ff8fea9e431f79a598070caf9afc4
Frasato <gabriel.frasato@hotmail.com>
2026-06-19 13:01:37 -0300

    feat: add endpoints

--- library-backend/src/main/java/com/library/library_backend/controllers/BookController.java
+++ library-backend/src/main/java/com/library/library_backend/controllers/BookController.java
@@ -1,16 +1,15 @@
 package com.library.library_backend.controllers;
 
-import com.library.library_backend.dto.ResponseGetBookIsbDto;
-import com.library.library_backend.mappers.BookMapper;
+import com.library.library_backend.dto.RequestIsbnDto;
+import com.library.library_backend.dto.ResponseBookDto;
 import com.library.library_backend.models.Book;
 import com.library.library_backend.services.BookFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/book")
@@ -18,14 +17,22 @@ public class BookController {
 
     @Autowired
     private BookFacade bookFacade;
-    @Autowired
-    private BookMapper bookMapper;
 
-    @GetMapping("/{isbn}")
-    public ResponseEntity<ResponseGetBookIsbDto> getByIsbn(@PathVariable("isbn") String isbn){
-        Book book = bookFacade.getInfosByIsbn(isbn);
-        ResponseGetBookIsbDto response = bookMapper.toDto(book);
+    @GetMapping
+    public ResponseEntity<List<Book>> allBooks(){
+        List<Book> response = bookFacade.allBooks();
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    @GetMapping("/{isbn}")
+    public ResponseEntity<ResponseBookDto> getByIsbn(@PathVariable("isbn") RequestIsbnDto requestIsbnDto){
+        ResponseBookDto response = bookFacade.getInfosByIsbn(requestIsbnDto.isbn());
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseBookDto> createBook(@RequestBody RequestIsbnDto requestIsbnDto){
+        ResponseBookDto response = bookFacade.createNewBook(requestIsbnDto.isbn());
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
