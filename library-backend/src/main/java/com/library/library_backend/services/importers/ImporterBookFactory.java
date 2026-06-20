b07b6b2c123e50c0f1206a146e848088961c419b
Frasato <gabriel.frasato@hotmail.com>
2026-06-20 12:04:23 -0300

    feat: add importer factory

---
+++ library-backend/src/main/java/com/library/library_backend/services/importers/ImporterBookFactory.java
@@ -0,0 +1,22 @@
+package com.library.library_backend.services.importers;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ImporterBookFactory {
+    private final List<BookImporter> importerList;
+
+    public ImporterBookFactory(List<BookImporter> importerList) {
+        this.importerList = importerList;
+    }
+
+    public BookImporter getImporter(String extension){
+        return importerList
+                .stream()
+                .filter(importer -> importer.supports(extension))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("File not supported!"));
+    }
+}
