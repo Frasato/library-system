9903ffc5a2b25d9493d9990932fcb9e316560fe1
Frasato <gabriel.frasato@hotmail.com>
2026-06-20 12:03:32 -0300

    feat: add query to fetch book by isbn list

--- library-backend/src/main/java/com/library/library_backend/repositories/BookRepository.java
+++ library-backend/src/main/java/com/library/library_backend/repositories/BookRepository.java
@@ -2,12 +2,23 @@ package com.library.library_backend.repositories;
 
 import com.library.library_backend.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, UUID> {
     Optional<Book> findByIsbn(String isbn);
+
+    @Query("""
+            SELECT b
+            FROM Book b
+            JOIN b.isbn i
+            WHERE i IN :isbn
+        """)
+    Optional<Book> findByIsbnList(@Param("isbn") List<String> isbn);
 }
