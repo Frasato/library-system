48db98e373ecc1ba359b50e7af9a13c40717a150
Frasato <gabriel.frasato@hotmail.com>
2026-06-20 21:24:27 -0300

    feat: install font and load themes

--- library-ui/src/main/java/org/frasato/Main.java
+++ library-ui/src/main/java/org/frasato/Main.java
@@ -1,9 +1,19 @@
 package org.frasato;
 
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+import javax.swing.*;
 import java.awt.*;
 
 public class Main {
     static void main() {
+        FlatRobotoFont.install();
+        UIManager.put("defaultFont", new Font(FlatRobotoFont.FAMILY, Font.PLAIN, 12));
+
+        FlatLaf.registerCustomDefaultsSource("raven.themes");
+
+        FlatMacLightLaf.setup();
         EventQueue.invokeLater(() -> new Application().setVisible(true));
     }
 }
