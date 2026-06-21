5fc786c01db0cbaa2088bc44498c2be9e3bf5e47
Frasato <gabriel.frasato@hotmail.com>
2026-06-20 22:52:26 -0300

    feat: create filter boilerplaate

---
+++ library-ui/src/main/java/org/frasato/view/home/components/Filter.java
@@ -0,0 +1,13 @@
+package org.frasato.view.home.components;
+
+import javax.swing.*;
+
+public class Filter extends JPanel {
+
+    public Filter(){ init(); }
+
+    public void init(){}
+
+    private JTextField filterField;
+    private JButton searchButton;
+}
