afa6fc3dd731b26a4b774c363e07bdf5ba1b6e09
Frasato <gabriel.frasato@hotmail.com>
2026-06-22 13:51:09 -0300

    feat: add text importer

---
+++ library-backend/src/main/java/com/library/library_backend/services/importers/TextBookImporter.java
@@ -0,0 +1,55 @@
+package com.library.library_backend.services.importers;
+
+import com.library.library_backend.models.Author;
+import com.library.library_backend.models.Book;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Component
+public class TextBookImporter implements BookImporter{
+    @Override
+    public boolean supports(String extension) { return extension.equals("txt"); }
+
+    @Override
+    public List<Book> importFile(MultipartFile file) {
+        List<Book> books = new ArrayList<>();
+
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
+            String line;
+
+            while((line = reader.readLine()) != null){
+                String[] values = line.split("\t");
+
+                Book book = new Book();
+                book.setTitulo(values[0].trim());
+                book.setDataPublicacao(values[1].trim());
+                book.setIsbn(List.of(values[2].trim()));
+                book.setEditora(List.of(values[3].trim()));
+
+                List<Author> authors = Stream.of(values[4].split(";"))
+                        .map(String::trim)
+                        .map(name -> {
+                            Author author = new Author();
+                            author.setNome(name);
+                            return author;
+                        })
+                        .toList();
+
+                book.setAuthor(authors);
+                books.add(book);
+            }
+
+            return books;
+
+        }catch(IOException e){
+            throw new RuntimeException("Error while importing text file! " + e);
+        }
+    }
+}
