764742c6b9874de97fa3216c2ba522b074a34525
Frasato <gabriel.frasato@hotmail.com>
2026-06-22 16:57:17 -0300

    feat: add logic to open modal when click

--- library-ui/src/main/java/org/frasato/controller/home/HomeController.java
+++ library-ui/src/main/java/org/frasato/controller/home/HomeController.java
@@ -4,8 +4,11 @@ import org.frasato.model.AuthorModel;
 import org.frasato.model.BookModel;
 import org.frasato.model.BookTableModel;
 import org.frasato.view.home.HomeView;
+import org.frasato.view.home.modal.EditModal;
+import org.frasato.view.home.modal.IncludeModal;
 
 import javax.swing.*;
+import java.io.File;
 import java.util.List;
 import java.util.UUID;
 
@@ -24,6 +27,31 @@ public class HomeController {
 
     private void init(){
         loadBooks();
+        bindEvents();
+    }
+
+    private void bindEvents(){
+        homeView.getBottom()
+                .getIncludeButton()
+                .addActionListener(e -> new IncludeModal().setVisible(true));
+
+        homeView.getBottom()
+                .getImportButton()
+                .addActionListener(e -> importFile());
+
+        homeView.getBottom()
+                .getEditButton()
+                .addActionListener(e -> new EditModal().setVisible(true));
+    }
+
+    private void importFile(){
+        JFileChooser chooser = new JFileChooser();
+        int result = chooser.showDialog(homeView, "Selecionar");
+
+        if(result == JFileChooser.APPROVE_OPTION){
+            File file = chooser.getSelectedFile();
+            System.out.println(file);
+        }
     }
 
     private void loadBooks(){
