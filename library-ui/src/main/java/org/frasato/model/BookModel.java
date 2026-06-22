963c6f7d37ef3e63ff94505631e24ec32a7136d3
Frasato <gabriel.frasato@hotmail.com>
2026-06-21 23:37:27 -0300

    feat: add book model

---
+++ library-ui/src/main/java/org/frasato/model/BookModel.java
@@ -0,0 +1,82 @@
+package org.frasato.model;
+
+import java.util.List;
+import java.util.UUID;
+
+public class BookModel {
+    private UUID id;
+    private String title;
+    private String publishDate;
+    private List<String> isbn;
+    private List<String> publishers;
+    private List<AuthorModel> authors;
+    private List<BookModel> similarBooks;
+
+    public BookModel() {}
+
+    public BookModel(UUID id, String title, String publishDate, List<String> isbn, List<String> publishers, List<AuthorModel> authors, List<BookModel> similarBooks) {
+        this.id = id;
+        this.title = title;
+        this.publishDate = publishDate;
+        this.isbn = isbn;
+        this.publishers = publishers;
+        this.authors = authors;
+        this.similarBooks = similarBooks;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public List<String> getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(List<String> isbn) {
+        this.isbn = isbn;
+    }
+
+    public List<String> getPublishers() {
+        return publishers;
+    }
+
+    public void setPublishers(List<String> publishers) {
+        this.publishers = publishers;
+    }
+
+    public List<AuthorModel> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<AuthorModel> authors) {
+        this.authors = authors;
+    }
+
+    public List<BookModel> getSimilarBooks() {
+        return similarBooks;
+    }
+
+    public void setSimilarBooks(List<BookModel> similarBooks) {
+        this.similarBooks = similarBooks;
+    }
+}
