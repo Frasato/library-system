b76e077c20e2e805469fc5393ccf4015fcfc53b9
Frasato <gabriel.frasato@hotmail.com>
2026-06-22 13:51:19 -0300

    feat: add xml importer

---
+++ library-backend/src/main/java/com/library/library_backend/services/importers/XmlBookImporter.java
@@ -0,0 +1,70 @@
+package com.library.library_backend.services.importers;
+
+import com.library.library_backend.models.Author;
+import com.library.library_backend.models.Book;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class XmlBookImporter implements BookImporter{
+
+    @Override
+    public boolean supports(String extension) {
+        return extension.equals("xml");
+    }
+
+    @Override
+    public List<Book> importFile(MultipartFile file) {
+        List<Book> books = new ArrayList<>();
+
+        try{
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(file.getInputStream());
+
+            document.getDocumentElement().normalize();
+            NodeList nodes = document.getElementsByTagName("book");
+
+            for(int i = 0; i < nodes.getLength(); i++){
+                Element element = (Element) nodes.item(i);
+
+                Book book = new Book();
+                book.setTitulo(getValue(element, "titulo"));
+                book.setDataPublicacao(getValue(element, "dataPublicacao"));
+                book.setIsbn(List.of(getValue(element, "item")));
+                book.setEditora(List.of(getValue((Element) element.getElementsByTagName("editora").item(0), "item")));
+
+                List<Author> authors = new ArrayList<>();
+
+                NodeList authorNodes = element.getElementsByTagName("author");
+
+                for(int j = 0; j < authorNodes.getLength(); j++){
+                    Element authorElement = (Element) authorNodes.item(j);
+                    Author author = new Author();
+                    author.setNome(getValue(authorElement, "nome"));
+                    authors.add(author);
+                }
+
+                book.setAuthor(authors);
+                books.add(book);
+            }
+
+            return books;
+
+        }catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getValue(Element element, String tag){
+        return element.getElementsByTagName(tag).item(0).getTextContent().trim();
+    }
+}
