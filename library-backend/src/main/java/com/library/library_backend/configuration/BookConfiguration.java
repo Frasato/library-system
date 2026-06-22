bd2c8b2264a546b73c0ba2c8947d8cd7e1435e06
Frasato <gabriel.frasato@hotmail.com>
2026-06-22 13:50:45 -0300

    feat: inject import service in facade

--- library-backend/src/main/java/com/library/library_backend/configuration/BookConfiguration.java
+++ library-backend/src/main/java/com/library/library_backend/configuration/BookConfiguration.java
@@ -5,6 +5,7 @@ import com.library.library_backend.mappers.BookMapper;
 import com.library.library_backend.repositories.AuthorRepository;
 import com.library.library_backend.repositories.BookRepository;
 import com.library.library_backend.services.*;
+import com.library.library_backend.services.importers.ImportBookService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -24,8 +25,8 @@ public class BookConfiguration {
     public AuthorByKey AuthorByKey(RestTemplate restTemplate, AuthorMapper authorMapper){ return new AuthorByKey(restTemplate, authorMapper);}
 
     @Bean
-    public BookFacade bookFacade(BookByIsbnService bookByIsbnService, AuthorByKey authorByKey, BookMapper bookMapper, CreateBookService createBookService, AllBooksService allBooksService, DeleteBookService deleteBookService, UpdateBookService updateBookService){
-        return new BookFacade(bookByIsbnService, authorByKey, bookMapper, createBookService, allBooksService, deleteBookService, updateBookService);
+    public BookFacade bookFacade(BookByIsbnService bookByIsbnService, AuthorByKey authorByKey, BookMapper bookMapper, CreateBookService createBookService, AllBooksService allBooksService, DeleteBookService deleteBookService, UpdateBookService updateBookService, ImportBookService importBookService){
+        return new BookFacade(bookByIsbnService, authorByKey, bookMapper, createBookService, allBooksService, deleteBookService, updateBookService, importBookService);
     }
 
     @Bean
