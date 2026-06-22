3510c7036ebc556e48130950063b5d1e56b69396
Frasato <gabriel.frasato@hotmail.com>
2026-06-21 23:38:30 -0300

    feat: add table component

--- library-ui/src/main/java/org/frasato/view/home/components/Table.java
+++ library-ui/src/main/java/org/frasato/view/home/components/Table.java
@@ -1,10 +1,34 @@
 package org.frasato.view.home.components;
 
+import com.formdev.flatlaf.FlatClientProperties;
+import net.miginfocom.swing.MigLayout;
+import org.frasato.model.BookTableModel;
 import javax.swing.*;
 
 public class Table extends JPanel {
 
-    public Table(){ init(); }
+    private JTable table;
 
-    public void init(){}
+    public Table(BookTableModel model){ init(model); }
+
+    public void init(BookTableModel model){
+
+        setLayout(new MigLayout("fill, insets 0", "[grow]", "[grow]"));
+        putClientProperty(FlatClientProperties.STYLE, "arc: 6;");
+
+        table = new JTable(model);
+
+        table.setRowHeight(40);
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+        table.setFillsViewportHeight(true);
+        table.getTableHeader().setReorderingAllowed(false);
+
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        add(scrollPane, "grow, push");
+    }
+
+    public JTable getTable(){
+        return table;
+    }
 }
