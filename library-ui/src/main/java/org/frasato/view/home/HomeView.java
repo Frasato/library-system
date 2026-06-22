9f729ee618dcdf70c0a0fb3a75efa6dc089126de
Frasato <gabriel.frasato@hotmail.com>
2026-06-21 23:38:52 -0300

    feat: add table and filter component

--- library-ui/src/main/java/org/frasato/view/home/HomeView.java
+++ library-ui/src/main/java/org/frasato/view/home/HomeView.java
@@ -1,16 +1,29 @@
 package org.frasato.view.home;
 
 import net.miginfocom.swing.MigLayout;
+import org.frasato.model.BookTableModel;
+import org.frasato.view.home.components.Filter;
 import org.frasato.view.home.components.Header;
+import org.frasato.view.home.components.Table;
 
 import javax.swing.*;
 
 public class HomeView extends JPanel {
 
-    public HomeView(){ init(); }
+    private Table table;
 
-    public void init(){
-        setLayout(new MigLayout("fill, insets 15", "[grow]", "[][pref!][grow][]"));
-        add(new Header());
+    public HomeView(BookTableModel model){ init(model); }
+
+    public void init(BookTableModel model){
+        setLayout(new MigLayout("fill", "[grow]", "[][][grow][]"));
+        add(new Header(), "wrap");
+        add(new Filter(), "growx, wrap");
+
+        table = new Table(model);
+        add(table);
+    }
+
+    public Table getTable(){
+        return this.table;
     }
 }
