4599f65289735b46d4b491a157c6f59bb44228c5
Frasato <gabriel.frasato@hotmail.com>
2026-06-23 10:03:35 -0300

    feat: add service to list all saved books

---
+++ library-ui/src/main/java/org/frasato/service/ListBooksService.java
@@ -0,0 +1,38 @@
+package org.frasato.service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.frasato.model.BookModel;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+
+
+public class ListBooksService {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public List<BookModel> execute(){
+        try{
+            HttpClient httpClient = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/v1/book"))
+                    .header("Content-Type","application/json")
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = httpClient
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            return mapper.readValue(
+                    response.body(),
+                    new TypeReference<List<BookModel>>(){}
+            );
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+}
