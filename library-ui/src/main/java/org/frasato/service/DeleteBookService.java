23064657de229296a1f977a0c6dd6afec9113514
Frasato <gabriel.frasato@hotmail.com>
2026-06-23 15:52:29 -0300

    feat: add delete book service

---
+++ library-ui/src/main/java/org/frasato/service/DeleteBookService.java
@@ -0,0 +1,30 @@
+package org.frasato.service;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.UUID;
+
+public class DeleteBookService {
+    public void execute(UUID bookId){
+        try{
+            HttpClient httpClient = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/v1/book/delete/" + bookId.toString()))
+                    .header("Content-Type","application/json")
+                    .DELETE()
+                    .build();
+
+            HttpResponse<String> response = httpClient
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            if(response.statusCode() != 200){
+                throw new RuntimeException("Error while deleting book: " + response.body());
+            }
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+}
