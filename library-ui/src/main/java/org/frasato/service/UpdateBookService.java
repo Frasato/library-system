2215e6058e1ec42ca97e5f046edd0bb09ba5ba19
Frasato <gabriel.frasato@hotmail.com>
2026-06-23 15:52:19 -0300

    feat: add update book service

---
+++ library-ui/src/main/java/org/frasato/service/UpdateBookService.java
@@ -0,0 +1,55 @@
+package org.frasato.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.frasato.dto.UpdateBookDTO;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Arrays;
+import java.util.List;
+
+public class UpdateBookService {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public String execute(
+            String title, String publishDate,
+            String isbn, String publisher,
+            String id
+    ){
+        if(title.isEmpty() || publishDate.isEmpty() || isbn.isEmpty() || publisher.isEmpty() || id.isEmpty()) return "Os campos não podem ser vazios!";
+
+        List<String> publishers = stringToList(publisher);
+        List<String> isbns = stringToList(isbn);
+
+        UpdateBookDTO requestUpdate = new UpdateBookDTO(title, publishDate, isbns, publishers, null);
+
+        try{
+            String body = mapper.writeValueAsString(requestUpdate);
+
+            HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/v1/book/" + id))
+                    .header("Content-Type","application/json")
+                    .method("PATCH", HttpRequest.BodyPublishers.ofString(body))
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if(response.statusCode() != 200) throw new RuntimeException("Error while updating book: " + response.body());
+
+            return "Atualizado com sucesso!";
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private List<String> stringToList(String item){
+        return Arrays
+                .stream(item.split(", "))
+                .map(String::trim)
+                .toList();
+    }
+}
