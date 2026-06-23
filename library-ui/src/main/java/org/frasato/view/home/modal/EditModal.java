d3dd8b596ac5455848ebd20ee1b2bc15bb38d98a
Frasato <gabriel.frasato@hotmail.com>
2026-06-23 10:02:35 -0300

    feat: create edit modal

--- library-ui/src/main/java/org/frasato/view/home/modal/EditModal.java
+++ library-ui/src/main/java/org/frasato/view/home/modal/EditModal.java
@@ -2,6 +2,7 @@ package org.frasato.view.home.modal;
 
 import com.formdev.flatlaf.FlatClientProperties;
 import net.miginfocom.swing.MigLayout;
+import org.frasato.view.home.components.EditModalTextField;
 import javax.swing.*;
 
 public class EditModal extends JDialog {
@@ -14,48 +15,72 @@ public class EditModal extends JDialog {
     private JTextField publisher;
     private JTextField author;
 
-    public EditModal() { init(); }
+    public EditModal() {
+        init();
+    }
 
-    private void init(){
-        setLayout(new MigLayout("fill", "[]", "[]"));
+    private void init() {
+
         setTitle("Editar Livro");
-        setSize(600, 500);
+        setSize(700, 550);
         setLocationRelativeTo(null);
 
+        setLayout(new MigLayout(
+                "fill, insets 40, align center center",
+                "[grow, center]",
+                "[]20[]10[]10[]10[]10[]20[]"
+        ));
+
         JLabel label = new JLabel("Informações sobre o livro");
+        label.putClientProperty(
+                FlatClientProperties.STYLE,
+                "font: +8"
+        );
 
-        title = new JTextField();
-
-        publishDate = new JTextField();
-
-        isbn = new JTextField();
-
-        publisher = new JTextField();
-
-        author = new JTextField();
+        title = new EditModalTextField("Título");
+        publishDate = new EditModalTextField("Data de publicação");
+        isbn = new EditModalTextField("ISBN");
+        publisher = new EditModalTextField("Editora");
+        author = new EditModalTextField("Autor");
 
         finishButton = new JButton("Concluir");
-        finishButton.putClientProperty(FlatClientProperties.STYLE, "arc: 6;" +
-                "minimumHeight: 30;" +
-                "hoverBackground: #bfdfff;"
+        finishButton.putClientProperty(
+                FlatClientProperties.STYLE,
+                """
+                arc:12;
+                minimumHeight:42;
+                minimumWidth:140;
+                hoverBackground:#bfdfff;
+                """
         );
 
         deleteButton = new JButton("Deletar");
-        deleteButton.putClientProperty(FlatClientProperties.STYLE, "arc: 6;" +
-                "foreground: #ff4a3d;" +
-                "minimumHeight: 30;" +
-                "hoverBackground: #ffd9d6;"
+        deleteButton.putClientProperty(
+                FlatClientProperties.STYLE,
+                """
+                arc:12;
+                foreground:#ff4a3d;
+                minimumHeight:42;
+                minimumWidth:140;
+                hoverBackground:#ffd9d6;
+                """
         );
 
         add(label, "wrap");
-        add(title, "wrap");
-        add(publishDate, "wrap");
-        add(isbn, "wrap");
-        add(publisher, "wrap");
-        add(author, "wrap");
-        add(finishButton);
-        add(deleteButton);
 
+        add(title, "w 350!, wrap");
+        add(publishDate, "w 350!, wrap");
+        add(isbn, "w 350!, wrap");
+        add(publisher, "w 350!, wrap");
+        add(author, "w 350!, wrap");
+
+        JPanel buttons = new JPanel(
+                new MigLayout("insets 0, gap 12", "[][]")
+        );
+
+        buttons.add(finishButton);
+        buttons.add(deleteButton);
+        add(buttons);
     }
 
     public JButton getFinishButton() {
