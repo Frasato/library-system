257f609225dc9b1733b26e61ec28508aa33f5505
Frasato <gabriel.frasato@hotmail.com>
2026-06-23 10:03:23 -0300

    feat: add book service to create a new book

---
+++ library-ui/src/main/java/org/frasato/service/AddBookService.java
@@ -0,0 +1,30 @@
+package org.frasato.service;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class AddBookService {
+    public void execute(String isbn){
+
+        String jsonBody = "{\"isbn\":\"" + isbn + "\"}";
+
+        try{
+            HttpClient httpClient = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/v1/book/create"))
+                    .header("Content-Type","application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+                    .build();
+
+            HttpResponse<String> response = httpClient
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println(response);
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+}
