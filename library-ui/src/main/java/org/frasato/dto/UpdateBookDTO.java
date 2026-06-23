6e7e58b9ed2bce5e96a6451df434936ae964301a
Frasato <gabriel.frasato@hotmail.com>
2026-06-23 15:51:28 -0300

    feat: add DTO to request update

---
+++ library-ui/src/main/java/org/frasato/dto/UpdateBookDTO.java
@@ -0,0 +1,11 @@
+package org.frasato.dto;
+
+import java.util.List;
+
+public record UpdateBookDTO(
+        String titulo,
+        String dataPublicacao,
+        List<String> isbn,
+        List<String> editora,
+        List<String> livrosSemelhantes
+){}
