d62e6c0c8ac23697a58ed304412d39b08249d622
Frasato <gabriel.frasato@hotmail.com>
2026-06-23 10:02:23 -0300

    feat: add edit modal text field component

---
+++ library-ui/src/main/java/org/frasato/view/home/components/EditModalTextField.java
@@ -0,0 +1,24 @@
+package org.frasato.view.home.components;
+
+import com.formdev.flatlaf.FlatClientProperties;
+import javax.swing.*;
+
+public class EditModalTextField extends JTextField {
+
+    public EditModalTextField(String placeholder){init(placeholder);}
+
+    private void init(String placeholder){
+        putClientProperty(
+                FlatClientProperties.PLACEHOLDER_TEXT,
+                placeholder
+        );
+
+        putClientProperty(
+                FlatClientProperties.STYLE,
+                """
+                arc:12;
+                minimumHeight:42;
+                """
+        );
+    }
+}
