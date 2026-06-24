baf5574efac1606d53b9a9d6a563d69e782b5b44
Frasato <gabriel.frasato@hotmail.com>
2026-06-24 14:36:22 -0300

    feat: add book not found exception

---
+++ library-backend/src/main/java/com/library/library_backend/exceptions/BookNotFoundException.java
@@ -0,0 +1,7 @@
+package com.library.library_backend.exceptions;
+
+public class BookNotFoundException extends RuntimeException {
+    public BookNotFoundException(String id) {
+        super("Book not found on: " + id);
+    }
+}
