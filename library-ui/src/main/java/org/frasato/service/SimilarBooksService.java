3e905ce952ce6a07d0642a7bbadea0d0239eff6b
Frasato <gabriel.frasato@hotmail.com>
2026-06-26 14:11:52 -0300

    feat: add similar books service to update only similar books in a book

---
+++ library-ui/src/main/java/org/frasato/service/SimilarBooksService.java
@@ -0,0 +1,51 @@
+package org.frasato.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.frasato.dto.UpdateBookDTO;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Arrays;
+import java.util.List;
+
+public class SimilarBooksService {
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public String execute(
+            List<String> booksId, String id
+    ){
+        if(booksId.isEmpty()) {
+            return "Selecione algum livro!";
+        }
+
+        UpdateBookDTO requestUpdate = new UpdateBookDTO(null, null, null, null, null, booksId);
+
+        try{
+            String body = mapper.writeValueAsString(requestUpdate);
+
+            HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/v1/book/" + id))
+                    .header("Content-Type","application/json")
+                    .method("PATCH", HttpRequest.BodyPublishers.ofString(body))
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if(response.statusCode() != 200) throw new RuntimeException("Error while updating book: " + response.body());
+
+            return "Atualizado com sucesso!";
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private List<String> stringToList(String item){
+        return Arrays
+                .stream(item.split(", "))
+                .map(String::trim)
+                .toList();
+    }
+}
