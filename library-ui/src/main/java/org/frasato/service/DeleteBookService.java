faf1df9d20cdb92525e5ba7e985f5bb0cf1ff6b7
Frasato <gabriel.frasato@hotmail.com>
2026-06-30 13:37:54 -0300

    refactor: change delete book http to web client

--- library-ui/src/main/java/org/frasato/service/DeleteBookService.java
+++ library-ui/src/main/java/org/frasato/service/DeleteBookService.java
@@ -1,12 +1,11 @@
 package org.frasato.service;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import org.springframework.web.reactive.function.client.WebClient;
 import java.util.UUID;
 
 public class DeleteBookService {
+
+    private final WebClient client = WebClient.create("http://localhost:8080");
 
     /**
      * Remove um livro existente via DELETE na API.
@@ -15,20 +14,12 @@ public class DeleteBookService {
      */
     public void execute(UUID bookId){
         try{
-            HttpClient httpClient = HttpClient.newHttpClient();
+            client.delete()
+                    .uri("/v1/book/delete/{id}", bookId.toString())
+                    .retrieve()
+                    .toBodilessEntity()
+                    .block();
 
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("http://localhost:8080/v1/book/delete/" + bookId.toString()))
-                    .header("Content-Type","application/json")
-                    .DELETE()
-                    .build();
-
-            HttpResponse<String> response = httpClient
-                    .send(request, HttpResponse.BodyHandlers.ofString());
-
-            if(response.statusCode() != 200){
-                throw new RuntimeException("Error while deleting book: " + response.body());
-            }
         }catch(Exception e){
             throw new RuntimeException(e);
         }
